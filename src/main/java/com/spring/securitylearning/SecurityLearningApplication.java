be8a3f153b4bb333fe9fc3fe761279c52af3ab5c
paderewska <iza.paderewska@gmail.com>
2019-04-28 14:13:33 +0200

    first commit

---
+++ src/main/java/com/spring/securitylearning/SecurityLearningApplication.java
@@ -0,0 +1,13 @@
+package com.spring.securitylearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityLearningApplication.class, args);
+	}
+
+}
