b29d25e68647b27f60f766c329e70804fef4a258
paderewska <iza.paderewska@gmail.com>
2019-05-08 20:02:51 +0200

    aop learning

--- src/main/java/com/spring/securitylearning/SecurityLearningApplication.java
+++ src/main/java/com/spring/securitylearning/SecurityLearningApplication.java
@@ -11,11 +11,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.UUID;
 
 @SpringBootApplication
@@ -42,6 +50,35 @@ public class SecurityLearningApplication {
 			Object object = pjp.proceed();
 			this.log.info("after " + pjp.toString());
 			return object;
+		}
+	}
+
+	@Component
+	class LoggingFilter implements javax.servlet.Filter {
+
+		private final Log log = LogFactory.getLog(getClass());
+
+		@Override
+		public void init(FilterConfig filterConfig) throws ServletException {
+		}
+
+		@Override
+		public void destroy() {
+		}
+
+		@Override
+		public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+			Assert.isTrue(servletRequest instanceof HttpServletRequest, "this assumes you have an HTTP request");
+
+			HttpServletRequest httpServletRequest = HttpServletRequest.class.cast(servletRequest);
+			String uri = httpServletRequest.getRequestURI();
+			this.log.info("new request for " + uri + ".");
+			long time = System.currentTimeMillis();
+			filterChain.doFilter(servletRequest, servletResponse);
+			long delta = System.currentTimeMillis() - time;
+			this.log.info("request for " + uri + " took " + delta + "ms");
+
 		}
 	}
 
